2044c98ab6daaae6e7049ef73a94f507385a917f
M4hbod <74229780+M4hbod@users.noreply.github.com>
2023-01-08 11:26:36 +0330

    change Button border

--- available_menus_frame.py
+++ available_menus_frame.py
@@ -33,10 +33,10 @@ class AvailableMenuFrames():
         self.app_name.grid(row=1, column=0, padx=10, pady=10, sticky='nswe')
 
 
-        self.download_button = ctk.CTkButton(self.options_frame, image=self.download_icon, compound='left', text="Downloads", corner_radius=15, border_width=2, width=40, font=font, border_color='#de4f4f', fg_color='#343638', command=lambda : self.content.toggleFrame("downloads"))
+        self.download_button = ctk.CTkButton(self.options_frame, image=self.download_icon, compound='left', text="Downloads", corner_radius=15, width=40, font=font, fg_color='#343638', command=lambda : self.content.toggleFrame("downloads"))
         self.download_button.grid(row=2, column=0, padx=5, pady=5)
 
 
-        self.settings_button = ctk.CTkButton(self.options_frame, image=self.settings_icon, compound='left', text="Settings", corner_radius=15, border_width=2, width=40, font=font, border_color='#2a8ce1', fg_color='#343638', command=lambda : self.content.toggleFrame("settings"))
+        self.settings_button = ctk.CTkButton(self.options_frame, image=self.settings_icon, compound='left', text="Settings", corner_radius=15, width=40, font=font, fg_color='#343638', command=lambda : self.content.toggleFrame("settings"))
         self.settings_button.grid(row=3, column=0, padx=5, pady=5)
        
