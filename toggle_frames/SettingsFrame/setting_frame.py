077a233a0174c821d8fab2ce5a9b7713fa01a270
M4hbod <74229780+M4hbod@users.noreply.github.com>
2023-01-08 12:01:45 +0330

    add style

--- toggle_frames/SettingsFrame/setting_frame.py
+++ toggle_frames/SettingsFrame/setting_frame.py
@@ -23,8 +23,14 @@ class SettingsFrame:
         self.database = self.getJSON(JSON_FILE_PATH)
 
         self.video_download_path = self.database['path']
+        self.style = self.database['style']
+        
         if self.video_download_path == "":
             self.video_download_path = f"{os.getcwd()}\Download".replace("\\", "/")
+        if self.style == "":
+            self.style = "Dark"
+        
+        ctk.set_appearance_mode(self.style)
 
 
 
@@ -56,26 +62,35 @@ class SettingsFrame:
         self.path_button.grid(row=0, column=2, padx=5, pady=5)
 
 
-
+        # Appearance Mode Label
+        self.appearance_mode_label = ctk.CTkLabel(self.options_frame, text="Style:", font=font, width=50)
+        self.appearance_mode_label.grid(row=1, column=0, padx=10, pady=(10, 10))
+        
+        # Appearance Mode OptionMenu -> Light, Dark, System
+        self.appearance_mode_optionemenu = ctk.CTkOptionMenu(self.options_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=1, column=1, padx=20, pady=(10, 10))
+        
+        
+        
+        # Set Default Values
+        self.appearance_mode_optionemenu.set(self.style)
 
 
 
     #============ Attributes/Functions ============
 
     #showFrame Function -> To show the frame by unforgetting the grid
-
     def showFrame(self):
         self.main_frame.grid(row=0, column=1, sticky='nswe', padx=7, pady=7)
 
 
     #hideFrame Function -> Ofc, the vise versa of showFrame function
-
     def hideFrame(self):
         self.main_frame.grid_forget()
 
 
     #Set path Function -> Select a path for downloading the video
-
     def setPath(self):
 
         path = filedialog.askdirectory()
@@ -95,24 +110,27 @@ class SettingsFrame:
             self.setJSON(JSON_FILE_PATH, "path", path)
 
     # Get JSON -> To get the data in the configs.json file
-
     def getJSON(self, filename):
 
         with open(filename, 'r') as data:
             all_data = json.load(data)
 
-            data.close()
-
         return all_data
 
     def setJSON(self, filename, key, value):
+        with open(filename, "r") as jsonFile:
+            data = json.load(jsonFile)
+        
+        data[key] = value
+        
+        with open(filename, "w") as jsonFile:
+            json.dump(data, jsonFile)
 
-        data_to_append = {key:value}
-        with open(filename, 'w') as data:
-            json.dump(data_to_append, data)
-
-            data.close()
-
+    # Change appearance mode event -> To change the appearance mode of the app
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        self.style = new_appearance_mode
+        self.setJSON(JSON_FILE_PATH, "style", self.style)
+        ctk.set_appearance_mode(self.style)
 
 
 
