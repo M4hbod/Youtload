e1875a84df8d70be379dd8fc22d8cb7c513663b2
M4hbod <74229780+M4hbod@users.noreply.github.com>
2023-01-08 23:26:54 +0330

    change in font

--- available_menus_frame.py
+++ available_menus_frame.py
@@ -6,7 +6,7 @@ from frames.DownloadVideoFrame.home_content_frame import ContentFrame
 
 class AvailableMenuFrames():
 
-    def __init__(self, master, font):
+    def __init__(self, master, text_font, tabs_font):
 
         
         self.options_frame = ctk.CTkFrame(master, width=150)
@@ -23,14 +23,14 @@ class AvailableMenuFrames():
         self.settings_icon = ctk.CTkImage(Image.open(r'Icons\LightModeImages\settings.png').resize((self.image_size, self.image_size), Image.NEAREST))
 
 
-        self.app_name = ctk.CTkLabel(self.options_frame, text="Tabs", font=font, text_color='#ffffff')
+        self.app_name = ctk.CTkLabel(self.options_frame, text="Youtload", font=tabs_font)
         self.app_name.grid(row=1, column=0, padx=10, pady=10, sticky='nswe')
 
 
-        self.download_button = ctk.CTkButton(self.options_frame, image=self.download_icon, compound='left', text="Downloads", corner_radius=15, width=40, font=font, fg_color='#343638', command=lambda : self.content.toggleFrame("downloads"))
+        self.download_button = ctk.CTkButton(self.options_frame, image=self.download_icon, compound='left', text="Downloads", corner_radius=15, width=40, font=text_font, fg_color='#343638', command=lambda : self.content.toggleFrame("downloads"))
         self.download_button.grid(row=2, column=0, padx=5, pady=5)
 
 
-        self.settings_button = ctk.CTkButton(self.options_frame, image=self.settings_icon, compound='left', text="Settings", corner_radius=15, width=40, font=font, fg_color='#343638', command=lambda : self.content.toggleFrame("settings"))
+        self.settings_button = ctk.CTkButton(self.options_frame, image=self.settings_icon, compound='left', text="Settings", corner_radius=15, width=40, font=text_font, fg_color='#343638', command=lambda : self.content.toggleFrame("settings"))
         self.settings_button.grid(row=3, column=0, padx=5, pady=5)
        
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from frames.SettingsFrame.setting_frame import SettingsFrame
 
 current_directory = directory.get_current_directory()
 files_and_folders = directory.get_files_and_folders(current_directory)
+
 if not directory.check_if_directory_exists('temp'):
     directory.create_directory('temp')
 
@@ -25,13 +26,14 @@ class MainApp:
 
         self.root.grid_columnconfigure(1, weight=1)
         self.root.grid_rowconfigure(0, weight=1)
-        self.main_font = ('Segoe UI Black', 10)
+        self.main_font = ('Segoe UI Black', 11)
+        self.big_font = ('Segoe UI Black', 15)
 
 
         #============ Options Frame ============
         # In self.root at 1st row and 1st column
         
-        self.option_frame = AvailableMenuFrames(self.root, self.main_font)
+        self.option_frame = AvailableMenuFrames(self.root, self.main_font, self.big_font)
 
 
 
